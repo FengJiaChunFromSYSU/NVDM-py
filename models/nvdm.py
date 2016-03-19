I think formula in paper is wrong...?

models/nvdm.py
@@ -8,8 +8,8 @@ class NVDM(Model):
   """Neural Varational Document Model"""
 
   def __init__(self, sess, reader, dataset="ptb",
-               batch_size=20, num_steps=3, embed_dim=500,
-               h_dim=50, learning_rate=0.01, max_iter=450000,
+               batch_size=20, embed_dim=500, h_dim=50,
+               learning_rate=0.001, max_iter=450000,
                checkpoint_dir="checkpoint"):
     """Initialize Neural Varational Document Model.
 
@@ -33,7 +33,7 @@ class NVDM(Model):
     self.step = tf.Variable(0, trainable=False)  
 
     self.dataset = "ptb"
-    self._attrs = ["h_dim", "embed_dim", "max_iter", "batch_size", "num_steps", "learning_rate"]
+    self._attrs = ["h_dim", "embed_dim", "max_iter", "batch_size", "learning_rate"]
 
     self.build_model()
 
@@ -47,10 +47,10 @@ class NVDM(Model):
     self.e_loss = -0.5 * tf.reduce_sum(1 + self.log_sigma_sq - tf.square(self.mu) - tf.exp(self.log_sigma_sq))
 
     # Log likelihood
-    self.g_loss = tf.reduce_sum(tf.log(self.p_x_i + 1e-10))
+    self.g_loss = -tf.reduce_sum(tf.log(self.p_x_i + 1e-10))
 
     self.loss = tf.reduce_mean(self.e_loss + self.g_loss)
-    self.optim = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(-self.loss)
+    self.optim = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self.loss)
 
     _ = tf.scalar_summary("encoder loss", self.e_loss)
     _ = tf.scalar_summary("decoder loss", self.g_loss)
@@ -67,9 +67,6 @@ class NVDM(Model):
 
       eps = tf.random_normal((1, self.h_dim), 0, 1, dtype=tf.float32)
       sigma = tf.sqrt(tf.exp(self.log_sigma_sq))
-
-      _ = tf.histogram_summary("mu", self.mu)
-      _ = tf.histogram_summary("sigma", sigma)
 
       self.h = self.mu + sigma * eps
 
@@ -108,20 +105,23 @@ class NVDM(Model):
         print("Step: [%4d/%4d] time: %4.4f, loss: %.8f, e_loss: %.8f, g_loss: %.8f" \
             % (step, self.max_iter, time.time() - start_time, loss, e_loss, g_loss))
 
-      if step % 1000 == 0:
+      if step % 100 == 0:
+        self.sample(5)
+
+      if step % 500 == 0:
         self.save(self.checkpoint_dir, step)
 
-  def sample(self):
+  def sample(self, sample_size=20):
     """Sample the documents."""
     p = 1
     x, word_idxs = self.reader.random()
     print " ".join([self.reader.idx2word[word_idx] for word_idx in word_idxs])
 
-    for idx in xrange(20):
+    for idx in xrange(sample_size):
       cur_ps = self.sess.run([self.p_x_i], feed_dict={self.x: x})
       cur_p, word_idx = np.max(cur_ps), np.argmax(cur_ps)
 
-      print self.reader.idx2word[word_idx], cur_p
+      print("[%d] %s\t\t: %.8f" % (idx+1, self.reader.idx2word[word_idx], cur_p))
       p *= cur_p
 
     print("perplexity : %8.f" % -np.log(p))