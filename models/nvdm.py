f955575be2aa6275c2efc66554d5d8a1a5b21da5
Taehoon Kim <carpedm20@gmail.com>
2016-03-19 19:10:16 +0900

    model finished

--- models/nvdm.py
+++ models/nvdm.py
@@ -29,26 +29,37 @@ class NVDM(Model):
     self.input_ = tf.placeholder(tf.float32, name="input")
     self.otuput = tf.placeholder(tf.float32, name="output")
 
-    self.loss = None
+    # Kullback Leibler divergence
+    self.encoder_loss = -0.5 * tf.reduce_sum(self.h_dim + self.log_sigma - tf.square(self.mu) - tf.exp(self.log_sigma))
+
+    # log likelihood
+    self.decoder_loss = tf.reduce_sum(tf.log(self.p_x_i))
+
+    self.cost = tf.reduce_mean(self.encoder_loss + self.decoder_loss)
+    self.optim = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self.cost)
 
   def build_encoder(self):
     """Inference Network. q(h|X)"""
-    l1 = tf.nn.relu(tf.nn.rnn_cell.linear(self.input_, self.embed_dim, bias=True))
-    l2 = tf.nn.relu(tf.nn.rnn_cell.linear(l1, self.embed_dim, bias=True))
+    with tf.variable_scope("encoder"):
+      l1 = tf.nn.relu(tf.nn.rnn_cell.linear(self.input_, self.embed_dim, bias=True))
+      l2 = tf.nn.relu(tf.nn.rnn_cell.linear(l1, self.embed_dim, bias=True))
 
-    self.mu = tf.nn.rnn_cell.linear(l2, self.h_dim, bias=True)
-    self.sigma = tf.exp(tf.rnn_cell.linear(l2, self.h_dim, bias=True))
+      self.mu = tf.nn.rnn_cell.linear(l2, self.h_dim, bias=True)
+      self.log_sigma = tf.rnn_cell.linear(l2, self.h_dim, bias=True)
 
-    eps = tf.random_normal((self.batch_size, self.h_dim), 0, 1, dtype=tf.float32)
-    self.h = self.mu + self.sigma * eps
+      eps = tf.random_normal((self.batch_size, self.h_dim), 0, 1, dtype=tf.float32)
+      self.h = self.mu + self.sigma * eps
 
   def build_decoder(self):
     """Inference Network. p(X|h)"""
-    R = tf.Variable()
-    b = tf.Variable()
-    e = tf.exp(-self.h * R * self.input_) + b
+    with tf.variable_scope("decoder"):
+      R = tf.get_variable("R", [self.vocab_size, self.hidden_dim])
+      b = tf.get_variable("b", [self.vocab_size])
 
-    p_x_h = tf.nn.softmax(e)
+      x_i = tf.diag([1]*self.vocab_size)
+
+      e = -tf.matmul(tf.matmul(self.h, R), x_i) + b
+      self.p_x_i = tf.nn.softmax(e)
 
   def train(self, config):
     pass
