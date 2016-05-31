4dee0ce68814f212d2ece7a9b289c41cc4c35cd6
Taehoon Kim <carpedm20@gmail.com>
2016-05-31 11:03:54 +0900

    File system scheme error fix. #1

--- models/base.py
+++ models/base.py
@@ -11,7 +11,7 @@ class Model(object):
     model_dir = self.dataset
     for attr in self._attrs:
       if hasattr(self, attr):
-        model_dir += "_%s:%s" % (attr, getattr(self, attr))
+        model_dir += "_%s=%s" % (attr, getattr(self, attr))
     return model_dir
 
   def save(self, checkpoint_dir, global_step=None):
